b91abe80846b1d69bab8e167b611f54896cbb97a
RASmith1 <bucknbeck@verizon.net>
2014-09-26 11:02:13 -0400

    Added README.md and directories and files for P1 for CSCI E15.

---
+++ README.md
@@ -0,0 +1,29 @@
+# ***CSCI E-15 Dynamic Web Applications***
+# ***Project P1: System Setup / Portfolio***
+ 
+# **Live URL**
+*http://p1.dynamicweb.me*
+
+# **Description sentences**
+P1: System Setup / Portfolio project introduces the student developer to and immerses them 
+in learning the industry-standard skills needed to develop a dynamic website.  These skills include
+downloading and installing environment software, such as local server and command-line editor; 
+configuring a local server; setting up Git on the client; creating an account on GitHub
+and enabling communication between the client and GitHub; creating an account on DigitalOcean
+and enabling communication between GitHub and Digital Ocean and enabling remote login from client 
+to Digital Ocean; tracking software and configuration changes on the client via Git and GitHub; 
+creating and cloning repositories; setting up a web domain; and implementing a web page that 
+is tracked using Git/GitHub and is deployed to Digital Ocean and viewable on the student's domain.
+
+# **Demo information** 
+Students are not being asked to do demos for this project.
+
+# **Details for Teaching Team**
+No login is required.
+On the web page, if you hover over the links, it will show you what the link is resolving to. 
+
+# **Outside Code**
+No outside code was used in this project.  I did use an image of vintage wallpaper/background for
+the background of my web page.  I don't remember where I got it from, but I used it for a web 
+project in CSCI E12, and I remember I went through the exercise of ensuring I didn't need to note 
+the source of it then.
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <title>
+        <h1>About Rebecca Smith</h1>
+    </title>
+
+    <link rel="stylesheet" href="css/about_me_style.css" type="text/css" />
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+</head>
+
+<body>
+
+    <div id="wrapper" class="container">
+
+        <p id="About_me_1st_line">Rebecca Smith</p>
+        <p id="About_me_2nd_line">Portfolio for CSCI E-15 Dynamic Web Applications</p>
+
+        <div class="center">
+            <img id="My_pics" class="masthead" src="images/My_mug.jpg" alt="Rebecca Smith's picture from her LinkedIn profile." width="100" height="150">
+        </div>
+        
+        <div class="linkscontainer">
+            <div class="row">
+                <div class="headercell">
+                    <h2>P2: PHP Basics</h2>
+                </div>
+                <div class="blankcell">
+                    <p></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="viewcell">View: <a href="http://p2.dynamicweb.me">P2: PHP Basics</a>
+                </div>
+                <div class="githubcell">GitHub: <a href="https://github.com/RASmith1/P2">P2: PHP Basics</a>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="headercell">
+                    <h2>P3: Laravel Basics</h2>
+                </div>
+                <div class="blankcell">
+                    <p></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="viewcell">View: <a href="http://p3.dynamicweb.me">P3: Laravel Basics</a>
+                </div>
+                <div class="githubcell">GitHub: <a href="https://github.com/RASmith1/P3">P3: Laravel Basics</a>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="headercell">
+                    <h2>P4: Final</h2>
+                </div>
+                <div class="blankcell">
+                    <p></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="viewcell">View: <a href="http://p4.dynamicweb.me">P4: Final</a>
+                </div>
+                <div class="githubcell">GitHub: <a href="https://github.com/RASmith1/P4">P4: Final</a>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="paragraphscontainer">
+            <ul id="paragraphs">
+                <li>
+                    <h2>Who I am and what I do</h2>
+                    <p>I was a software engineer up until 13 years ago, when my daughter was born. Now I'm earning my Web Technologies Certificate through Harvard Extension School in an effort to get back into software development.</p>
+                    <br/>
+                </li>
+                <li>
+                    <h2>Why I'm taking this course</h2>
+                    <p>I'm taking this particular course because Professor Buck has included in this course many of the aspects of web development that I'm lacking in my skill set, i.e., LAMP.</p>
+                    <br/>
+                </li>
+                <li>
+                    <h2>My previous experience with programming</h2>
+                    <p>I graduated long ago from University of Maryland with a BS in Comp.Sci. Since then, up until 13 years ago, I programmed in Ada and C mostly. I also did Perl and shell scripting.
+                    </p>
+                    <br/>
+                </li>
+                <li>
+                    <h2>Platform</h2>
+                    <p>I'm doing this course work on a Dell Latitude 6400 with Windows 7.
+                    </p>
+                    <br/>
+                </li>
+                <li>
+                    <h2>Anything else you should know?</h2>
+                    <p>I love web development!  
+                    </p>
+                    <br/>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
